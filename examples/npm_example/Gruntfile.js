2cb28f94be0d1d43bf4182994016ad356add2d58
Camilo Rincon <carincon@gmail.com>
2018-09-22 11:13:39 -0500

    Ejemplo de minify archivos con grunt

--- examples/npm_example/Gruntfile.js
+++ examples/npm_example/Gruntfile.js
@@ -1,8 +1,20 @@
 module.exports =function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    autor:'NextU'
+    autor:'NextU',
+    uglify:{
+      dist:{
+        files:{
+            'build/js/<%= pkg.name %>.min.js':[
+                'src/js/main.js'
+            ]
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default',['uglify']);
 
   grunt.registerTask('Hola','mi primera tarea registrada',function(){
     grunt.log.writeln("Hola esta es mi primera tarea por "+grunt.config('autor'));
