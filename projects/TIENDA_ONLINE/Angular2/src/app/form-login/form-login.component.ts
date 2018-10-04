726f5c925a74f22b2ad417d02c462f9936335a08
Camilo Rincon <carincon@gmail.com>
2018-10-03 21:51:28 -0500

    Validación de formulario pantalla inicial

--- projects/TIENDA_ONLINE/Angular2/src/app/form-login/form-login.component.ts
+++ projects/TIENDA_ONLINE/Angular2/src/app/form-login/form-login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FormLoginComponent implements OnInit {
 
   enviarForm(form){
-    console.log(form);
+    console.log(form.value.email_input,form.value.password_input);
   }
 
   ngOnInit() {
