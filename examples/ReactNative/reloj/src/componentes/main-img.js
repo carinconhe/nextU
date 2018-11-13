3714a7a937c56e32215f404addcc0f7b20078d18
Camilo Rincon <carincon@gmail.com>
2018-11-12 19:01:47 -0500

    imagen desde url y desde el directorio local

--- examples/ReactNative/reloj/src/componentes/main-img.js
+++ examples/ReactNative/reloj/src/componentes/main-img.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 
 const MainImg = () => {
     return (
-        <Image 
-            style={styles.imageStyles}
-            source={{ uri: 'https://cdn.pixabay.com/photo/2015/11/07/11/21/city-1031046_640.jpg' }} 
-        />
+        <View style={styles.viewStyles}>
+            <Image 
+                style={styles.imageStyles}
+                source={{ uri: 'https://cdn.pixabay.com/photo/2015/11/07/11/21/city-1031046_640.jpg' }} 
+            />
+            <Image
+                style={styles.imageStyles}
+                source={require('../images/Bogota.jpg')}
+            />
+        </View>
     );
 };
 
 const styles = {
     imageStyles: {
-        height: 200,
+        height: 300,
         flex: 1,
-        width: null
+        display :'flex',
+        flexDirection: 'column',
+        width: null,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+    },
+    viewStyles: {
+        flexGrow: 1
     }
 };
 
