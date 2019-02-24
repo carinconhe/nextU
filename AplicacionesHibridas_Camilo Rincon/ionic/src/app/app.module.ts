98f40c06e4fb9d17ea6115587dc052493f7b7887
Camilo Rincon <camilo.rincon@barbara.net.co>
2019-02-24 15:01:22 -0500

    Se agregar el proyecto final, proyecto de parse, la base de datos de parse

---
+++ AplicacionesHibridas_Camilo Rincon/ionic/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegistroPage } from '../pages/registro/registro';
+import { ComentariosPage } from '../pages/comentarios/comentarios';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegistroPage,
+    ComentariosPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegistroPage,
+    ComentariosPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ AplicacionesHibridas_Camilo Rincon/ionic/src/user-model.ts
@@ -0,0 +1,6 @@
+export interface User{
+    username: string;
+    password: string;
+    name?: string;  //El caracter ? quiere indicar que es opcional.
+    email?: string; //El caracter ? quiere indicar que es opcional.   
+}
